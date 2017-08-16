39a48fd80a2a24f15d6734d625a5639fb312d9f5
Paul Sobolik <psobolik@live.com>
2017-08-15 23:08:14 -0400

    Open comic by number and other tweaks

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,26 +1,30 @@
 'use strict'
 
+const {shell} = require('electron')
+
 angular // eslint-disable-line
   .module('xkcdViewerApp', [])
-  .controller('MainController', ['$scope', '$http', ($scope, $http) => {
-    var latestComic
-    $scope.error = $scope.imageInfo = null
+  .controller('MainController', ['$scope', '$http', '$anchorScroll', ($scope, $http, $anchorScroll) => {
     var loadComic = (n) => {
+      $scope.error = 'Loading...'
+      $scope.imageInfo = {}
       var url = getUrl(n)
       $http.get(url)
       .then((data) => {
         console.log(data)
         var imageInfo = data.data
-        if (n === 0) latestComic = imageInfo
+        if (n === 0) $scope.latestComicNum = imageInfo.num
         $scope.imageInfo = imageInfo
         imageInfo.date = new Date(imageInfo.year, imageInfo.month - 1, imageInfo.day)
         $scope.error = null
         $scope.showingFirstComic = imageInfo.num === 1
-        $scope.showingLastComic = imageInfo.num === latestComic.num
+        $scope.showingLastComic = imageInfo.num === $scope.latestComicNum
       })
       .catch((e) => {
         $scope.error = 'Error getting comic.'
+        $scope.showingFirstComic = $scope.showingLastComic = false
       })
+      $anchorScroll('top')
     }
     var getUrl = (n) => {
       let folder = n > 0 ? (n + '/') : ''
@@ -47,12 +51,23 @@ angular // eslint-disable-line
     $scope.loadLastComic = (event) => {
       event && event.preventDefault()
       if (!$scope.showingLastComic) {
-        loadComic(latestComic.num)
+        loadComic(0)
       }
     }
     $scope.loadRandomComic = (event) => {
       event && event.preventDefault()
-      loadComic(Math.floor((Math.random() * latestComic.num) + 1))
+      loadComic(Math.floor((Math.random() * $scope.latestComicNum) + 1))
+    }
+    $scope.loadComic = (event, n) => {
+      event && event.preventDefault()
+      if (n < 1) $scope.loadFirstComic()
+      else if (n > $scope.latestComicNum) $scope.loadLastComic()
+      else loadComic(n)
+    }
+    $scope.gotoXkcd = (event) => {
+      event && event.preventDefault()
+      let url = `https://xkcd.com/${$scope.imageInfo.num}`
+      shell.openExternal(url)
     }
     // Load the latest comic to start
     loadComic(0)
